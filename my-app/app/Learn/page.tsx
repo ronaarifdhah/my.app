161a849293c3960e86c7d1d38eef5f05b6e9f9e0
ronaarifdhah <ronaa.rifdhah@gmail.com>
2026-04-20 12:37:33 +0800

    first commit

---
+++ my-app/app/Learn/page.tsx
@@ -0,0 +1,16 @@
+
+export default function learnpage() {
+    return (
+    <div>
+        <div>
+            <img src="/palette.svg" alt="Design" />
+            <p>Design</p>
+        </div>
+        <div>
+            <p>Figma</p>
+            <p>8 Hours ago</p>
+        </div>
+    </div>
+);
+
+}
