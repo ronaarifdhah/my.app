bac26db598c269d1374d372e0a13881c10960d3d
ronaarifdhah <ronaa.rifdhah@gmail.com>
2026-04-27 12:30:36 +0800

    update css

--- my-app/app/Learn/page.tsx
+++ my-app/app/Learn/page.tsx
@@ -1,16 +1,41 @@
 
 export default function learnpage() {
     return (
-    <div>
-        <div>
-            <img src="/palette.svg" alt="Design" />
-            <p>Design</p>
+    <section className="h-screen bg-gray-100 flex gap-4">
+        <div className="bg-purple-300 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-purple-900">
+            <div className="flex gap-4">
+                <img src="/palette.svg" alt="Design" />
+                <p className="font-bold">Design</p>
+            </div>
+            <div>
+                <p className="text-4xl font-bold">Figma</p>
+                <p className="font-bold">8 Hours ago</p>
+            </div>
         </div>
-        <div>
-            <p>Figma</p>
-            <p>8 Hours ago</p>
+
+        <div className="bg-orange-200 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-orange-800">
+            <div className="flex gap-4">
+                <img src="/code.svg" alt="Coding" />
+                <p className="font-bold">Coding</p>
+            </div>
+            <div>
+                <p className="text-4xl font-bold">Python</p>
+                <p className="font-bold">2 Days ago</p>
+            </div>
         </div>
-    </div>
+
+        <div className="bg-red-300 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-red-900">
+            <div className="flex gap-4">
+                <img src="/palette.svg" alt="Design" />
+                <p className="font-bold">Design</p>
+            </div>
+            <div>
+                <p className="text-4xl font-bold">Sketch</p>
+                <p className="font-bold">4 Days ago</p>
+            </div>
+        </div>
+    </section>
+
 );
 
 }
